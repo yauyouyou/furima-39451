423555083a9d835373557edca74afe57a882cbd4
yauyouyou <iwaiwa1115@gmail.com>
2023-06-19 21:26:11 +0900

    ログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email, presence: true, uniqueness: true
-  
 end
