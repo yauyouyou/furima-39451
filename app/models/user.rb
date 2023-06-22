49fe14f4e30de6ee9c6f8ebe4241011dd5a0f69c
yauyouyou <iwaiwa1115@gmail.com>
2023-06-22 19:19:16 +0900

    レビュー修正➀

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :lastname_zenkaku, presence: true
   validates :firstname_zenkaku, presence: true
-  validates :lastname_zenkaku, format: { with: /\A[一-龥ぁ-んァ-ン]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
-  validates :firstname_zenkaku, format: { with: /\A[一-龥ぁ-んァ-ン]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :lastname_zenkaku, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :firstname_zenkaku, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :lastname_kana_zenkaku, presence: true
   validates :firstname_kana_zenkaku, presence: true
-  validates :lastname_kana_zenkaku, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）で入力してください' }
-  validates :firstname_kana_zenkaku, format: { with: /\A[ァ-ン]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :lastname_kana_zenkaku, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :firstname_kana_zenkaku, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角（カタカナ）で入力してください' }
   validates :birthday, presence: true
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get'items',to:'items#index'
-  post'items',to:'items#create'
+  # post'items',to:'items#create'
   root to: 'items#index'
 end
