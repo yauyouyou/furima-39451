78e3e9997b7e5cd7d88cbc8109779e37f76dd0fd
yauyouyou <iwaiwa1115@gmail.com>
2023-06-26 16:42:23 +0900

    テーブル作成・バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,26 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  belongs_to :user
+
+  validates :item_name, presence: true
+  validates :price, presence: true
+  validates :item_explain, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :until_sendday_id, presence: true
+  validate :validate_image_presence
+
+  private
+
+  def validate_image_presence
+    return if image.attached?
+
+    errors.add(:image, 'must be attached')
+  end
 end
+
+
+:item_name, :price, :item_explain, :category_id, :item_condition_id, :delivery_charge_id, :prefecture_id, :until_sendday_id).merge(user_id: current_user.id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_051529) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_071326) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.integer "price", null: false
+    t.text "item_explain", null: false
+    t.integer "category_id", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "delivery_charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "until_sendday_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
