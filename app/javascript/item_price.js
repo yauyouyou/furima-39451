7c2d605ccb3fdc50784dea7526e6ba8462f00a6a
yauyouyou <iwaiwa1115@gmail.com>
2023-06-27 15:57:53 +0900

    販売利益実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,21 +1,29 @@
-const priceInput = document.getElementById("item-price");
-const addTaxDom = document.getElementById("add-tax-price");
+document.addEventListener("DOMContentLoaded", () => {
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById('profit');
 
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value.trim();
-  console.log(inputValue);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value.trim();
+    console.log(inputValue);
 
-  if (inputValue === "") {
-    addTaxDom.innerHTML = "";
-    return;
-  }
+    if (inputValue === "") {
+      addTaxDom.innerHTML = "";
+      profitDom.innerHTML = "";
+      return;
+    }
 
-  const price = parseInt(inputValue);
-  if (isNaN(price)) {
-    addTaxDom.innerHTML = "";
-    return;
-  }
+    const price = parseInt(inputValue);
+    if (isNaN(price)) {
+      addTaxDom.innerHTML = "";
+      profitDom.innerHTML = "";
+      return;
+    }
 
-  const tax = Math.floor(price * 0.1);
-  addTaxDom.innerHTML = tax.toLocaleString();
+    const tax = Math.floor(price * 0.1);
+    const profit = price - tax;
+
+    addTaxDom.innerHTML = tax.toLocaleString();
+    profitDom.innerHTML = profit.toLocaleString();
+  });
 });
