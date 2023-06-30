a82d5d05dce748106f83ce419aaf84b843fe574e
yauyouyou <iwaiwa1115@gmail.com>
2023-06-30 19:49:50 +0900

    手順2仮実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.save
+      # 保存成功時の処理
+      redirect_to root_path
+    else
+      # 保存失敗時の処理
+      render :index
+    end
+  end
+
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:user, :item, :post_code, :prefecture_id, :municipality, :address, :telephone_number, :building_name)
+  end
 end
